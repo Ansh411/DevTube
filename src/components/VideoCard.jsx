6b927972ecd2c9521fa81ee99a4e1112acc04ba9
Ansh411 <4gansh11@gmail.com>
2025-12-27 02:05:02 +0530

    Created these cards and fetched most popular youtube videos using the API

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,40 @@
+import ChannelAvatar from "./ChannelAvatar";
+import { formatViews, timeAgo } from "../utils/helpers";
 
-const VideoCard = () => {
+const VideoCard = ({ info }) => {
+  const { snippet, statistics } = info;
+  const { title, channelTitle, channelId, thumbnails, publishedAt } = snippet;
+
   return (
-    <div>VideoCard</div>
+    <div className="cursor-pointer transition-transform duration-300 ease-out hover:scale-[1.05]">
+      {/* THUMBNAIL */}
+      <div className="relative">
+        <img
+          src={thumbnails?.high?.url}
+          alt="thumbnail"
+          className="w-full rounded-xl object-cover aspect-video"
+        />
+      </div>
+
+      {/* INFO */}
+      <div className="flex gap-3 mt-3">
+        <ChannelAvatar channelId={channelId} />
+
+        <div className="flex flex-col">
+          <h3 className="text-sm font-semibold line-clamp-2 leading-snug">
+            {title}
+          </h3>
+
+          <p className="text-xs font-semibold hover:text-gray-900 text-gray-600 mt-1">
+            {channelTitle}
+          </p>
+
+          <p className="text-xs font-semibold  text-gray-600">
+            {formatViews(statistics?.viewCount)} • {timeAgo(publishedAt)}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,22 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import VideoCard from "./VideoCard";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
 
-  useEffect(() => { 
-    getVideos(); 
-  },[]);
+  useEffect(() => {
+    getVideos();
+  }, []);
 
   const getVideos = async () => {
-
-    const data = await fetch(YOUTUBE_VIDEOS_API);
-    const json = await data.json();
-    console.log(json.items);
-  }
+    try {
+      const res = await fetch(YOUTUBE_VIDEOS_API);
+      const json = await res.json();
+      setVideos(json.items || []);
+    } catch (err) {
+      console.error("Failed to fetch videos", err);
+    }
+  };
 
   return (
-    <div><VideoCard/></div>
+    <section className="px-6 py-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {videos.map((video) => (
+          <Link key={video.id} to={"/watch?v=" + video.id}>
+            <VideoCard info={video} />
+          </Link>
+        ))}
+      </div>
+    </section>
   );
 };
 
