59d823ac113bc7ff50688a598a2c617279fd9e0a
Ansh411 <4gansh11@gmail.com>
2025-12-28 19:41:40 +0530

    Added Search functionality with clicking on tags

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,7 +1,9 @@
 
-const Tag = ({ name, active = false }) => {
+const Tag = ({ name, active, onClick }) => {
   return (
-    <button className={`shrink-0 px-4 py-2 whitespace-nowrap rounded-full text-sm font-medium cursor-pointer transition-all duration-200
+    <button
+        onClick={() => onClick(name)} 
+        className={`shrink-0 px-4 py-2 whitespace-nowrap rounded-full text-sm font-medium cursor-pointer transition-all duration-200
         ${active ? "bg-black text-white" : "bg-gray-100 text-gray-800 hover:bg-gray-200"}`}>
       {name}
     </button>
