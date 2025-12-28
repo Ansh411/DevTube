6a58063646d2cd55aba6d430236aabc109504bbf
Ansh411 <4gansh11@gmail.com>
2025-12-28 19:39:48 +0530

    Added some APIs for search videos and also the video data with id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,18 @@
 const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key=" + YOUTUBE_API_KEY;
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=28&regionCode=IN&key=" + YOUTUBE_API_KEY;
 
 export const YOUTUBE_CHANNEL_API = (channelId) => `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${YOUTUBE_API_KEY}`;
 
-export const YOUTUBE_SEARCH_API = "https://devtube-backend-8ffj.onrender.com/api/search?q=";
+export const YOUTUBE_SEARCH_SUGGESTIONS_API = "https://devtube-backend-8ffj.onrender.com/api/search?q=";
 
 export const YOUTUBE_VIDEO_DATA_API = (videoId) => `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${YOUTUBE_API_KEY}`;
 
-export const YOUTUBE_COMMENTS_API = (videoId) => `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=${videoId}&maxResults=100&key=${YOUTUBE_API_KEY}`;
+export const YOUTUBE_COMMENTS_API = (videoId) => `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=${videoId}&maxResults=25&key=${YOUTUBE_API_KEY}`;
+
+export const YOUTUBE_SEARCH_VIDEOS_API = (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=28&q=${encodeURIComponent(query)}&key=${YOUTUBE_API_KEY}`;
+
+export const YOUTUBE_VIDEOS_BY_IDS_API = (ids) => `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${ids}&key=${YOUTUBE_API_KEY}`;
 
 
 export const Tags = [
