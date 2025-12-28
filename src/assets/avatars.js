4a8d1bdc79e8e2339621135aa00634d2acc70d93
Ansh411 <4gansh11@gmail.com>
2025-12-28 19:46:24 +0530

    Added some random avatars for Channel Avatar if there are no avatar or if there is any issue in loading the avatars

---
+++ src/assets/avatars.js
@@ -0,0 +1,22 @@
+export const avatars = [
+
+"https://i.pinimg.com/1200x/92/b4/e7/92b4e7c57de1b5e1e8c5e883fd915450.jpg",
+"https://i.pinimg.com/736x/b0/d8/8c/b0d88c8fb4a462b3b75c82df5cd455c5.jpg",
+"https://i.pinimg.com/1200x/53/17/d0/5317d0da1852623f281a056deed7188d.jpg",
+"https://i.pinimg.com/736x/80/98/35/80983566c704fe079168178606dd79e1.jpg",
+"https://i.pinimg.com/736x/be/63/5e/be635e844cf106b57ee689803dca58af.jpg",
+"https://i.pinimg.com/736x/4c/fb/ce/4cfbcef5bcaf7587b1df8cc82c28bc0d.jpg",
+"https://i.pinimg.com/1200x/9e/ef/ac/9eefacfcbdb27ce516bbfccbc93e3413.jpg",
+"https://i.pinimg.com/736x/f0/d2/82/f0d2822867cc7d5ad3f0aa5e16466e2f.jpg",
+"https://i.pinimg.com/736x/3c/94/af/3c94af037b04666d04572505c87adfa0.jpg",
+"https://i.pinimg.com/736x/87/d2/39/87d239c65732f941a8f2d9cce9f245f9.jpg",
+"https://i.pinimg.com/1200x/e7/5c/53/e75c535fffa3f1a6046c5a05ed491271.jpg",
+"https://i.pinimg.com/736x/91/80/bc/9180bcf3564ff6b6f7daa0c6b89214ea.jpg",
+"https://i.pinimg.com/736x/f5/08/f5/f508f5b7feab52fa03294025fc8fe404.jpg",
+"https://i.pinimg.com/736x/e2/63/2c/e2632c0c8cdf58d9ac1d4ff0bf7d1fc2.jpg",
+"https://i.pinimg.com/736x/dc/12/15/dc121517a443373e26c63135af0422a7.jpg",
+"https://i.pinimg.com/1200x/75/f3/3d/75f33d47e1b0afc6630d13c514f12519.jpg",
+"https://i.pinimg.com/736x/3e/a2/a7/3ea2a74c3107f0f3e201fa0c0bf7a2ce.jpg",
+"https://i.pinimg.com/1200x/9c/e9/2a/9ce92a2effca3b2561bebf8a0ca35d57.jpg",
+
+];
--- src/components/ChannelAvatar.jsx
+++ src/components/ChannelAvatar.jsx
@@ -1,23 +1,45 @@
 import { useEffect, useState } from "react";
 import { YOUTUBE_CHANNEL_API } from "../utils/constants";
+import { avatars } from "../assets/avatars";
+
+/* ---------- STABLE RANDOM ---------- */
+const getFallbackAvatar = (channelId) => {
+  if (!channelId) return avatars[0];
+  const index = channelId.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  return avatars[index % avatars.length];
+};
 
 const ChannelAvatar = ({ channelId }) => {
   const [logo, setLogo] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchChannel = async () => {
-      const res = await fetch(YOUTUBE_CHANNEL_API(channelId));
-      const json = await res.json();
-      const thumbnails = json?.items?.[0]?.snippet?.thumbnails;
-      setLogo(thumbnails?.high?.url || thumbnails?.medium?.url || thumbnails?.default?.url);
+      try {
+        const res = await fetch(YOUTUBE_CHANNEL_API(channelId));
+        const json = await res.json();
+
+        const thumbnails = json?.items?.[0]?.snippet?.thumbnails;
+        const img = thumbnails?.high?.url || thumbnails?.medium?.url || thumbnails?.default?.url || null;
+
+        setLogo(img);
+      } catch (err) {
+        setError(true);
+      }
     };
 
-    fetchChannel();
+    if (channelId) fetchChannel();
   }, [channelId]);
 
+  const fallback = getFallbackAvatar(channelId);
+
   return (
-    <img src={logo} 
-    alt="channel logo" className="h-9 w-9 rounded-full object-cover"
+    <img
+      src={!error && logo ? logo : fallback}
+      alt="channel avatar"
+      className="h-9 w-9 lg:h-10 lg:w-10 rounded-full object-cover"
+      loading="lazy"
+      onError={() => setError(true)}
     />
   );
 };
